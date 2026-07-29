93e44aa59e26ad0fb84896a8d1f4bdb767862ad9
Rakhee <rsmistry05061996@gmail.com>
2026-07-29 13:59:45 +0530

    Feat:Reactangle functionality

---
+++ Rectangle.ts
@@ -0,0 +1,21 @@
+class Rectangle{
+    length1:number;
+    width1:number;
+
+    constructor(length:number,breadth:number){
+        this.length1 = length;
+        this.width1 = breadth;
+    }
+
+    area(){
+        return this.length1*this.width1;
+    }
+
+    perimeter(){
+        return 2*(this.length1+this.width1);
+    }
+}
+
+let rect = new Rectangle(5,7);
+console.log("Area of Rectangle is",rect.area());
+console.log("Perimeter of Rectangle is",rect.perimeter());
