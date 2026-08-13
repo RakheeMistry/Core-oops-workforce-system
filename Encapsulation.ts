dcd7f550a6fc2a5b32bfabe73b70286b643ce6db
Rakhee <rsmistry05061996@gmail.com>
2026-08-13 20:07:00 +0530

    encapsulated properties with hard private fields

--- Encapsulation.ts
+++ Encapsulation.ts
@@ -1,3 +1,4 @@
+// Encapsulation with Access Modifiers in TypeScript
 class BankAccount {
     private balance: number;
 
@@ -27,6 +28,30 @@ class BankAccount {
 }
 
 const account = new BankAccount(1000);
-account.deposit(500);
-account.withdraw(200);
-console.log(`Current balance: ${account.getBalance()}`);
+account.deposit(500); // Deposited: 500. New balance: 1500
+account.withdraw(200); // Withdrew: 200. New balance: 1300
+console.log(`Current balance: ${account.getBalance()}`); // Current balance: 1300
+
+// ❌ TypeScript Compilation Errors:
+// console.log(account.balance);    // Property 'balance' is protected
+
+
+// Encapsulation with Hard Private Fields (#field) vs TypeScript (private)
+class User {
+    private tsPrivate: string = "TS Private";
+    #jsPrivate: string = "JS Hard Private";
+
+    getSecret(){
+        return this.#jsPrivate; // Internal access to JS private field
+    }
+}
+
+const user = new User();
+
+console.log(user.getSecret()); // Accessing JS private field through a method
+
+// Error: Property 'tsPrivate' is private and only accessible within class 'User'. TS Private: Throws a TypeScript compiler error, but still exists at runtime.
+console.log(user.tsPrivate); 
+
+// Error: Property '#jsPrivate' is a private field and only accessible within class 'User'. Hard Private: Causes a JavaScript Runtime SyntaxError if accessed directly
+//console.log(user.#jsPrivate);
--- README.md
+++ README.md
@@ -11,3 +11,12 @@ npx ts-node <file-name.ts>
 ------------------------------------------------------------------------------------------------
 Note: npx is used because the TypeScript is installed locally for the particular project folder
 ------------------------------------------------------------------------------------------------
+
+
+* Encapsulation : Hard Private Fields (#field) vs TypeScript private
+--------------------------------------------------------------------------------------------------
+TypeScript offers two ways to make fields private:
+
+1.TypeScript private modifier: Enforced only at compile time. Once compiled to JavaScript, the property becomes public in the emitted code.
+
+2.ES2022 Private Fields (# prefix): Enforced at runtime by JavaScript itself. The field cannot be accessed outside the class under any circumstances, even at runtime.
