808af7699e01716711372dadc23af2bbabe627c6
Rakhee <rsmistry05061996@gmail.com>
2026-09-07 19:36:51 +0530

    polymorphism of typescript

--- Polymorphism.ts
+++ Polymorphism.ts
@@ -1,27 +1,27 @@
 // Method Overriding(Runtime Polymorphism)
-// class Animal{
-//     makeSound(): void{
-//         console.log("Animal makes a sound");
-//     }
-// }
+class Animal{
+    makeSound(): void{
+        console.log("Animal makes a sound");
+    }
+}
 
-// class Dog extends Animal{
-//     makeSound(): void {
-//         console.log("Dog says: Bark");
-//     }
-// }
+class Dog extends Animal{
+    makeSound(): void {
+        console.log("Dog says: Bark");
+    }
+}
 
-// class Cat extends Animal{
-//     makeSound(): void {
-//         console.log("Cat says: Meow");
-//     }
-// }
+class Cat extends Animal{
+    makeSound(): void {
+        console.log("Cat says: Meow");
+    }
+}
 
-// const a1: Animal = new Dog();
-// const a2: Animal = new Cat();
+const a1: Animal = new Dog();
+const a2: Animal = new Cat();
 
-// a1.makeSound();
-// a2.makeSound();
+a1.makeSound();
+a2.makeSound();
 
 // Notice this line:
 // * const a1: Animal = new Dog(); *
@@ -30,36 +30,35 @@
 
 
 // Real-world runtime polymorphism example 
+abstract class LoginService {
+    abstract login(): void;
+}
 
-// abstract class LoginService {
-//     abstract login(): void;
-// }
+class GoogleLogin extends LoginService{
+    login(): void{
+        console.log("Login with Google");
+    }
+}
 
-// class GoogleLogin extends LoginService{
-//     login(): void{
-//         console.log("Login with Google");
-//     }
-// }
+class FacebookLogin extends LoginService{
+    login(): void {
+        console.log("Login with Facebook");
+    }
+}
 
-// class FacebookLogin extends LoginService{
-//     login(): void {
-//         console.log("Login with Facebook");
-//     }
-// }
+class GithubLogin extends LoginService{
+    login(): void{
+        console.log("Login with Github");
+    }
+}
 
-// class GithubLogin extends LoginService{
-//     login(): void{
-//         console.log("Login with Github");
-//     }
-// }
+function authenticate(service: LoginService){
+    service.login();
+}
 
-// function authenticate(service: LoginService){
-//     service.login();
-// }
-
-// authenticate(new GoogleLogin());
-// authenticate(new FacebookLogin());
-// authenticate(new GithubLogin());
+authenticate(new GoogleLogin());
+authenticate(new FacebookLogin());
+authenticate(new GithubLogin());
 
 // Method Overloading(Compile-time Polymorphism)
 abstract class Logger{
